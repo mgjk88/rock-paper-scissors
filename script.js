ad75c09c879a40da9ce48dabe46eaad40751f009
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2024-12-04 22:56:21 +0800

    fix getHumanChoice, create playRound
    getHumanChoice now case insensitive. playRound
    created to update score and console each rounc.

--- script.js
+++ script.js
@@ -4,19 +4,42 @@ let computerScore = 0;
 function getComputerChoice(){
     const number = Math.round((Math.random() * 10)) % 3;
     return  number === 0 
-    ? "Rock" 
+    ? "paper" 
     : number === 1
-        ? "Paper"
-        : "Scissors";
+        ? "scissors"
+        : "rock";
 }
 
 function getHumanChoice(){
-    const input = prompt("Enter Rock, Paper or Scissors: ");
-    if(input === "Rock" || input === "Paper" || input === "Scissors"){
+    const input = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
+    if(input === "rock" || input === "scissors" || input === "paper"){
         return input;
     }
     else {
         console.log("Invalid Answer!")
         return getHumanChoice();
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    function choiceTable(choice){
+        switch(choice){
+            case "rock": return 1;
+            case "scissors": return 0;
+            case "paper": return -1;
+        }
+    }
+
+    const outcome = choiceTable(humanChoice) - choiceTable(computerChoice);
+    if((outcome > 0 && outcome !== 2)||  outcome === -2){
+        humanScore++;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}!`);
+    }
+    else if((outcome < 0 && outcome !== -2) || outcome === 2){
+        computerScore++;
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
+    }
+    else{
+        console.log(`Draw!`)
     }
 }
