da39deaadb097374c8b5668893ae58941d8ce5dd
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2024-12-04 22:10:00 +0800

    Create getHumanChoice function
    function prompts for human choice of Rock, Paper
    or Scissors, else it returns invalid answer and
    continues prompting the human for an answer.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function getComputerChoice(){
     const number = Math.round(Math.random() * 3);
     return  number === 1 
-    ? "rock" 
+    ? "Rock" 
     : number === 2
-        ? "paper"
-        : "scissors";
+        ? "Paper"
+        : "Scissors";
+}
+
+function getHumanChoice(){
+    const input = prompt("Enter Rock, Paper or Scissors: ");
+    if(input === "Rock" || input === "Paper" || input === "Scissors"){
+        return input;
+    }
+    else {
+        console.log("Invalid Answer!")
+        return getHumanChoice();
+    }
 }
