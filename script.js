41b199ce70c027bfd80ba2ab72d23e4bee216164
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2024-12-04 22:19:55 +0800

    initialise scores, redesign getComputerChoice
    scores are intialised as 0 for human and computer.
    getComputerChoice logic is to ensure a more random
    outcome

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    const number = Math.round(Math.random() * 3);
-    return  number === 1 
+    const number = Math.round((Math.random() * 10)) % 3;
+    return  number === 0 
     ? "Rock" 
-    : number === 2
+    : number === 1
         ? "Paper"
         : "Scissors";
 }
